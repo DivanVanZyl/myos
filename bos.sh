c10da7a036c1836ab6489d965a17f70b0f58681d
Divan van Zyl <divanvanzyl@capitecbank.co.za>
2019-03-27 14:28:41 +0200

    Added function that prints a border around a single-line string.

--- bos.sh
+++ bos.sh
@@ -2,10 +2,26 @@
 
 cd /home/divan/myos
 
+echo "Clean Start"
+
 ./clean.sh
+
+echo "Clean Stop"
+
+echo "Headers Start"
 
 ./headers.sh
 
+echo "Headers Stop"
+
+echo "iso Start"
+
 ./iso.sh
 
+echo "iso Stop"
+
+echo "qemu Start"
+
 ./qemu.sh
+
+echo "qemu Stop"
--- myos/kernel/kernel/kernel.c
+++ myos/kernel/kernel/kernel.c
@@ -4,5 +4,18 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+	
+	/*
+	To do
+	
+	Learn interrupts. (This is needed if I'm going to read input from the user.)
+	Create a Shell.
+	Paging, memory management.
+	Create a kernel-space multitasking system. Context switch to user mode.
+	Process scheduling.
+	Virtual memory.
+	Implement file system.
+	*/
+
+	printfWithBorder("Anja hou baie van mooi borders  (:");
 }
